101f9a6fb82ac4b9a3dbcc22b6bfbc8e1f56cb26
musicis3965 <41126317+musicis3965@users.noreply.github.com>
2020-01-09 11:34:56 -0500

    Add files via upload

---
+++ js/indexOld.20200109113247.js
@@ -0,0 +1,16 @@
+(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery);
+$.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof attr !== typeof undefined && attr !== false; };
+
+
+$(document).ready(function() {
+r=function(){dpi=window.devicePixelRatio;$('.js30').attr('src', (dpi>1) ? 'images/img_3633-1302.jpg' : 'images/img_3633-651.jpg');
+};
+if(!window.HTMLPictureElement){r();}
+(function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(i,e){$(e).click(function(){var t=e.hash.length>1?$('[name="'+e.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
+var wl = new woolite();
+wl.init();
+wl.addAnimation($('.js28')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js29')[0], "1.00s", "0.00s", 1, 100);
+wl.start();
+
+});
