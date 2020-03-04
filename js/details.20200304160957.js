785a1919c77c783382db8c71d7ca0bb8d5cc5f9e
musicis3965 <41126317+musicis3965@users.noreply.github.com>
2020-03-04 16:12:45 -0500

    Add files via upload

---
+++ js/details.20200304160957.js
@@ -0,0 +1,36 @@
+(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery);
+$.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof attr !== typeof undefined && attr !== false; };
+
+
+$(document).ready(function() {
+r=function(){dpi=window.devicePixelRatio;$('.js25').attr('src', (dpi>1) ? 'images/5cb7832ca7c7755bf004c148-1078.png' : 'images/5cb7832ca7c7755bf004c148-539.png');
+
+
+
+};
+if(!window.HTMLPictureElement){r();}
+(function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(i,e){$(e).click(function(){var t=e.hash.length>1?$('[name="'+e.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
+var wl = new woolite();
+wl.init();
+wl.addAnimation($('.js12')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js13')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js14')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js15')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js16')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js17')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js18')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js19')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js20')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js21')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js22')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js23')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js24')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js25')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js26')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js27')[0], "1.30s", "0.00s", 1, 100);
+wl.addAnimation($('.js28')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js29')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js30')[0], "1.00s", "0.00s", 1, 100);
+wl.start();
+
+});
---
+++ js/index.20200304160957.js
@@ -0,0 +1,15 @@
+(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery);
+$.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof attr !== typeof undefined && attr !== false; };
+
+
+$(document).ready(function() {
+r=function(){dpi=window.devicePixelRatio;};
+if(!window.HTMLPictureElement){r();}
+var wl = new woolite();
+wl.init();
+wl.addAnimation($('.js')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js2')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js3')[0], "1.00s", "0.00s", 1, 100);
+wl.start();
+
+});
